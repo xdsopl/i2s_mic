3dc082e439cfd24b3f4fef299d3461dd4f65087f
Ahmet Inan <xdsopl@gmail.com>
2023-02-02 13:38:47 +0100

    made constants static constexpr

--- agc.hh
+++ agc.hh
@@ -9,10 +9,10 @@ Copyright 2023 Ahmet Inan <xdsopl@gmail.com>
 template <typename TYPE, int BITS, int HOLD>
 class AGC
 {
-	const TYPE val_min = -(1 << (BITS - 1));
-	const TYPE val_max = (1 << (BITS - 1)) - 1;
-	const TYPE neg_mid = -(1 << (BITS - 2));
-	const TYPE pos_mid = (1 << (BITS - 2)) - 1;
+	static constexpr TYPE val_min = -(1 << (BITS - 1));
+	static constexpr TYPE val_max = (1 << (BITS - 1)) - 1;
+	static constexpr TYPE neg_mid = -(1 << (BITS - 2));
+	static constexpr TYPE pos_mid = (1 << (BITS - 2)) - 1;
 	int shift = 0;
 	int count = 0;
 public:
