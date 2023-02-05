81eefdf859e218b11f9d0817244cb9e4443888d1
Ahmet Inan <xdsopl@gmail.com>
2023-02-05 09:06:20 +0100

    use unsigned types for positive values

--- schmidl_cox.hh
+++ schmidl_cox.hh
@@ -17,17 +17,19 @@ class SchmidlCox {
 	static constexpr int match_len = guard_len | 1;
 	static constexpr int match_del = (match_len - 1) / 2;
 	SMA<cmplx, cmplx16, symbol_len> cor;
-	SMA<value, int16_t, 2 * symbol_len> pwr;
-	SMA<value, int8_t, match_len> match;
+	SMA<value, uint16_t, 2 * symbol_len> pwr;
+	SMA<value, uint16_t, match_len> match;
 	Delay<cmplx, cmplx8, symbol_len> delay;
-	Clipper<cmplx, -127, 127> clip8;
-	Clipper<cmplx, -32767, 32767> clip16;
+	Clipper<cmplx, -127, 127> clip_s8;
+	Clipper<cmplx, 0, 255> clip_u8;
+	Clipper<cmplx, -32767, 32767> clip_s16;
+	Clipper<cmplx, 0, 65536> clip_u16;
 public:
 	value operator()(cmplx iq) {
-		iq = clip8(iq);
-		cmplx P = cor(clip16(delay(iq) * conj(iq)));
-		value R = pwr(clip16(norm(iq)));
-		value timing = match(clip8(norm((P << 9) / R))) >> 16;
+		iq = clip_s8(iq);
+		cmplx P = cor(clip_s16(delay(iq) * conj(iq)));
+		value R = pwr(clip_u16(norm(iq)));
+		value timing = match(clip_u16(norm((P << 9) / R))) >> 16;
 		return timing;
 	}
 };
